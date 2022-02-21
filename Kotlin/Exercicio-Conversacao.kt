b4d2916547a8c5f0fa3f43dc554ff33575edb443
Steve Harris <speed.harriss@gmail.com>
2022-02-21 14:19:48 -0300

    kotlintest

---
+++ Kotlin/Exercicio-Conversacao.kt
@@ -0,0 +1,32 @@
+fun main() {
+
+    //Exemplo Criativo de Conversa
+    //println = escrever linha, pulando pra proxima.
+
+    //println("Ola, Tudo bem?")
+    //println("tudo e voce?")
+
+    println("Henrique X NerdMaster Pt. 1")
+    println("NerdMaster: Finalmente, um oponente digno...")
+    println("Henrique: Quem é você?")
+    println("NerdMaster: Eu sou voce... So que senior...")
+    println("Henrique: Voce se corrompeu ao usar o poder do Kotlin!")
+    println("NerdMaster: VOCE E FRACO ! LHE FALTA CODIGO!")
+    println("Henrique: mas... eu tenho um Github personalizado com jogo da cobrinha ;-;")
+    println("NerdMaster: O uso do Chrome lhe enfraquece Henrique, nao limite o seu poder a tao pouco")
+    println("Henrique: Pense nos seus alunos, o que eles irao pensar quando descobrirem que voce se corrompeu a todo esse poder?")
+    println("NerdMaster: Meus alunos se foram a muito tempo, Liza, Generation, todos ficaram pra tras")
+    println("Henrique: O QUE VOCE FEZ?!?")
+    println("NerdMaster: Usei o codigo corrompido, descobri a forca por tras  do uso restrito do chrome, desafie-se e encontre a verdade")
+    println("Henrique: Olhe pra voce, se corrompeu por tao pouco, e o que voce tem agora? onde estao todos que lhe ajudaram a chegar ate aqui?")
+    println("NerdMaster: Nunca precisei deles, era fracos, e limitavam meu poder")
+    println("NerdMaster: Ignorancia. e Fatal ! MUWAHAHAHAH!")
+    println("**Henrique acorda assustado**")
+    println("Henrique: Ufa, foi so um sonho...")
+    println("CONTINUA...")
+
+
+
+
+
+}
